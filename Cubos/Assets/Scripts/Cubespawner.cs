0dc169288537ba033763ba9909d19ba8903e83d8
Jonathan Ivan <Jonathan.ivan1945@gmail.com>
2022-06-18 19:43:39 -0500

    verificacion

--- Cubos/Assets/Scripts/Cubespawner.cs
+++ Cubos/Assets/Scripts/Cubespawner.cs
@@ -11,20 +11,23 @@ public class Cubespawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject Objeto = Instantiate(prefabcubo);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GameObject Objeto = Instantiate(prefabcubo);
+        prefabcubo.transform.position = new Vector3(Random.Range(1, 5), Random.Range(6, 12), Random.Range(13, 20));
+        prefabcubo.GetComponent<MeshRenderer>().material.color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
     }
 
     private void OnEnable()
     {
         GameObject Objeto = Instantiate(prefabcubo);
         prefabcubo.transform.position = new Vector3(Random.Range(1, 5), Random.Range(6, 12), Random.Range(13, 20));
-        prefabcubo.GetComponent<MeshRenderer>().material.color = new(Random.Range(0.0f,1.0f), Random.Range(0.0f,1.0f), Random.Range(0.0f,1.0f));
+        prefabcubo.GetComponent<MeshRenderer>().material.color = new Color (Random.Range(0.0f,1.0f), Random.Range(0.0f,1.0f), Random.Range(0.0f,1.0f));
 
 
 
