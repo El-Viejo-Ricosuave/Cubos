a27bc760fec4fe3d5eec9e6a3c2aeeb363b261e6
Jonathan Ivan <Jonathan.ivan1945@gmail.com>
2022-07-24 16:40:24 -0500

    se genera componente

---
+++ Cubos/Assets/Scripts/Componentes.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Componentes : MonoBehaviour
+{
+  public Colores colores;
+  public Cubo1 cubo1;
+  public Cubo2 cubo2; 
+  public Cubo3 cubo3;
+  public Cubo4 cubo4;
+}
